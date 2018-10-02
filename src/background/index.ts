11cf01e0f79937e9f5ea88204021e3012865f338
Fredrik Wollsén <fredrik@neam.se>
2018-10-03 02:11:58 +0300

    feat: Import reusable instrumentation from the OpenWPM repository
    (at the time of the merge of https://github.com/citp/OpenWPM/pull/221)

---
+++ src/background/index.ts
@@ -0,0 +1,6 @@
+// export * from "./lib/RequestMonitor";
+import * as cookieInstrument from './cookie-instrument.js';
+import * as jsInstrument from './javascript-instrument.js';
+import * as httpInstrument from './http-instrument.js';
+
+export const background = { cookieInstrument, jsInstrument, httpInstrument };
